15317a1d3668cbdcb786764b251e6f6afb3a9a56
mrandyron <cevallos.andy@gmail.com>
2020-07-02 23:38:41 -0400

    Se agrega configuracion para CORS

--- src/main/java/com/yo/minimal/rest/security/WebSecurity.java
+++ src/main/java/com/yo/minimal/rest/security/WebSecurity.java
@@ -16,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -67,4 +70,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+
+     @Bean
+     public CorsFilter corsFilter() {
+         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+         CorsConfiguration config = new CorsConfiguration();
+         config.setAllowCredentials(true);
+         config.addAllowedOrigin("*");
+         config.addAllowedHeader("*");
+         config.addAllowedMethod("OPTIONS");
+         config.addAllowedMethod("GET");
+         config.addAllowedMethod("POST");
+         config.addAllowedMethod("PUT");
+         config.addAllowedMethod("DELETE");
+         source.registerCorsConfiguration("/**", config);
+         return new CorsFilter(source);
+     }
 }
