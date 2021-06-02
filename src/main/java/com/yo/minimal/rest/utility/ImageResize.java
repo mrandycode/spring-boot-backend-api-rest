3ae1f3f2b92066175eef2426bf95ce43caf2f4c9
mrandyron <cevallos.andy@gmail.com>
2021-06-01 20:37:42 -0400

    Se sube cambio en application.properties y ImageResize

--- src/main/java/com/yo/minimal/rest/utility/ImageResize.java
+++ src/main/java/com/yo/minimal/rest/utility/ImageResize.java
@@ -5,7 +5,6 @@ import com.yo.minimal.rest.dto.ResponseJ;
 import org.imgscalr.Scalr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
